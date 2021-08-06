bc5c630be3e9429c9487db5dacb58ec38c82d2d5
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2021-08-06 11:14:43 +0200

    start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-	    String[] colour = {"Blue", "Red", "Green", "Yellow"};
+	    String[] colour = {"Blue", "Red", "Green", "Yellow", "Purple", "Black", "White", "Grey"};
+	    Random random = new Random();
+	    int randomColour = random.nextInt(colour.length);
+	    System.out.println(randomColour);
+	    System.out.println(colour[randomColour]);
     }
 }
