1a58dcc3699ac6b9416982a6d8464d68aca2e408
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2021-08-06 11:21:51 +0200

    random

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,15 @@ public class Main {
     public static void main(String[] args) {
 	    String[] colour = {"Blue", "Red", "Green", "Yellow", "Purple", "Black", "White", "Grey"};
 	    Random random = new Random();
-	    int randomColour = random.nextInt(colour.length);
-	    System.out.println(randomColour);
-	    System.out.println(colour[randomColour]);
+	    int randomColour1 = random.nextInt(colour.length);
+	    int randomColour2 = random.nextInt(colour.length);
+	    int randomColour3 = random.nextInt(colour.length);
+	    int randomColour4 = random.nextInt(colour.length);
+	    System.out.println(randomColour1);
+	    System.out.println(colour[randomColour1]);
+		System.out.println(colour[randomColour2]);
+		System.out.println(colour[randomColour3]);
+		System.out.println(colour[randomColour4]);
     }
+
 }
