65b322c0b4479a9048ee5618b67ab9e4836924d5
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2021-08-06 16:06:13 +0200

    FINISH

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,22 +14,23 @@ public class Main {
 
         do {
             //colour generator
-            String[] colour = {"Blue", "Red", "Green", "Yellow", "Purple", "Black", "White", "Grey"};
+            String[] colour = {"Blau", "Rot", "Grün", "Gelb", "Violet", "Schwarz", "Weiss", "Grau"};
             Random random = new Random();
             int rc1 = random.nextInt(colour.length);
             int rc2 = random.nextInt(colour.length);
             int rc3 = random.nextInt(colour.length);
             int rc4 = random.nextInt(colour.length);
 
-            System.out.println(colour[rc1]);
-            System.out.println(colour[rc2]);
-            System.out.println(colour[rc3]);
-            System.out.println(colour[rc4]);
+            //System.out.println(colour[rc1]);
+            //System.out.println(colour[rc2]);
+            //System.out.println(colour[rc3]);
+            //System.out.println(colour[rc4]);
 
             String randomColour1 = colour[rc1];
             String randomColour2 = colour[rc2];
             String randomColour3 = colour[rc3];
             String randomColour4 = colour[rc4];
+            System.out.println("Die verfügenbaren Farben Blau, Rot, Grün, Gelb, Violet, Schwarz, Weiss, Grau");
             int num = 0;
             for (int i = 12; i > num; i--) {
 
@@ -38,6 +39,11 @@ public class Main {
 
                 if (i == 0) {
                     System.out.println("Verloren, Sie haben alle Ihre Versuche aufgebraucht!");
+                    System.out.println("Der Farbcode war:");
+                    System.out.println(randomColour1);
+                    System.out.println(randomColour2);
+                    System.out.println(randomColour3);
+                    System.out.println(randomColour4);
                 }
                 scanner();
                 i = Abgleichung(randomColour1, randomColour2, randomColour3, randomColour4, colour1, colour2, colour3, colour4, i);
@@ -73,28 +79,28 @@ public class Main {
         int vollKorrekt = 0;
 
 
-        if (colour1.equals(randomColour1) || colour1.equals(randomColour2) || colour1.equals(randomColour3) || colour1.equals(randomColour4)) {
+        if (colour1.equalsIgnoreCase(randomColour1) || colour1.equalsIgnoreCase(randomColour2) || colour1.equalsIgnoreCase(randomColour3) || colour1.equalsIgnoreCase(randomColour4)) {
             korrekt++;
         }
-        if (colour2.equals(randomColour1) || colour2.equals(randomColour2) || colour2.equals(randomColour3) || colour2.equals(randomColour4)) {
+        if (colour2.equalsIgnoreCase(randomColour1) || colour2.equalsIgnoreCase(randomColour2) || colour2.equalsIgnoreCase(randomColour3) || colour2.equalsIgnoreCase(randomColour4)) {
             korrekt++;
         }
-        if (colour3.equals(randomColour1) || colour3.equals(randomColour2) || colour3.equals(randomColour3) || colour3.equals(randomColour4)) {
+        if (colour3.equalsIgnoreCase(randomColour1) || colour3.equalsIgnoreCase(randomColour2) || colour3.equalsIgnoreCase(randomColour3) || colour3.equalsIgnoreCase(randomColour4)) {
             korrekt++;
         }
-        if (colour4.equals(randomColour1) || colour4.equals(randomColour2) || colour4.equals(randomColour3) || colour4.equals(randomColour4)) {
+        if (colour4.equalsIgnoreCase(randomColour1) || colour4.equalsIgnoreCase(randomColour2) || colour4.equalsIgnoreCase(randomColour3) || colour4.equalsIgnoreCase(randomColour4)) {
             korrekt++;
         }
-        if (colour1.equals(randomColour1)) {
+        if (colour1.equalsIgnoreCase(randomColour1)) {
             vollKorrekt++;
         }
-        if (colour2.equals(randomColour2)) {
+        if (colour2.equalsIgnoreCase(randomColour2)) {
             vollKorrekt++;
         }
-        if (colour3.equals(randomColour3)) {
+        if (colour3.equalsIgnoreCase(randomColour3)) {
             vollKorrekt++;
         }
-        if (colour4.equals(randomColour4)) {
+        if (colour4.equalsIgnoreCase(randomColour4)) {
             vollKorrekt++;
         }
 
@@ -114,7 +120,7 @@ public class Main {
         System.out.println("Möchtest du nochmals spielen? (Ja, Nein)");
         Scanner scanner = new Scanner(System.in);
         String rs = scanner.next();
-        if (rs.equals("Ja")) {
+        if (rs.equalsIgnoreCase("Ja")) {
             restart = true;
         } else {
             restart = false;
