be4bc254c30384e8f18054100f795a4382d48f79
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2021-08-06 11:49:04 +0200

    random

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,26 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+    	//colour generator
 	    String[] colour = {"Blue", "Red", "Green", "Yellow", "Purple", "Black", "White", "Grey"};
 	    Random random = new Random();
-	    int randomColour1 = random.nextInt(colour.length);
-	    int randomColour2 = random.nextInt(colour.length);
-	    int randomColour3 = random.nextInt(colour.length);
-	    int randomColour4 = random.nextInt(colour.length);
-	    System.out.println(randomColour1);
-	    System.out.println(colour[randomColour1]);
-		System.out.println(colour[randomColour2]);
-		System.out.println(colour[randomColour3]);
-		System.out.println(colour[randomColour4]);
+	    int rc1 = random.nextInt(colour.length);
+	    int rc2 = random.nextInt(colour.length);
+	    int rc3 = random.nextInt(colour.length);
+	    int rc4 = random.nextInt(colour.length);
+
+		String randomColour1 = colour[rc1];
+		String randomColour2 = colour[rc2];
+		String randomColour3 = colour[rc3];
+		String randomColour4 = colour[rc4];
+
+		Abgleichung(randomColour1, randomColour2, randomColour3, randomColour4);
+
     }
 
+    public static void Abgleichung(String randomColour1, String randomColour2, String randomColour3, String randomColour4) { //scanner einfügen
+    	if (scanner1 == ) {
+
+		}
+	}
 }
